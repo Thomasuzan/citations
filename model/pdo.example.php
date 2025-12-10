14b13ca2dbd08e5e9d0c930099d66e2f20d9555d
Thomasuzan <thomasuzanpro@gmail.com>
2025-12-10 17:37:57 +0100

    Add: pdo.example

---
+++ model/pdo.example.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * Renvoie un objet PDO
+ * 
+ * INSTRUCTIONS :
+ * 1. Copiez ce fichier et renommez-le en "pdo.php"
+ * 2. Remplacez les valeurs par vos identifiants de base de données
+ * 
+ * @param string $dsn destination
+ * @param string $user utilisateur de la base de données
+ * @param string $password mot de passe de l'utilisateur de la base de données
+ * @return PDO l'objet PDO de connexion à la base de données
+ */
+function getPdo(
+    string $dsn = 'mysql:host=VOTRE_SERVEUR;dbname=VOTRE_NOM_BDD',
+    string $user = 'VOTRE_UTILISATEUR',
+    string $password = 'VOTRE_MOT_DE_PASSE'
+): PDO {
+    try {
+        $db = new PDO($dsn, $user, $password);
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+        return $db;
+    } catch (PDOException $e) {
+        die('Probleme avec la base de données : ' . $e->getMessage());
+    }
+}
